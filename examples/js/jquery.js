6a0dfd2b58a78fc103102845c0cf29629f079df7
Chloe Hwang <chloehwang@MacBook-Pro.local>
2022-09-25 01:37:35 -0700

    Added Examples Folder and pdf

---
+++ examples/js/jquery.js
@@ -0,0 +1,34 @@
+
+$(()=>{
+
+    // ACCORDION
+
+    $(".accordion dt").on("click",function(e){
+        // $(this).next().showw();
+        // $(this).next().toggle();
+        // $(this).next().slideDown();
+        //$(this).next().slideToggle();
+        $(this).next().slideDown()
+            .siblings("dd").slideUp();
+    });
+
+
+    //TAB GROUP
+    $(".tabgroup .tab").on("click",function(e){
+
+        let id = $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active");
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active");
+
+
+    })
+
+
+
+});
+
